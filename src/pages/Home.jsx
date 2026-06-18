27cefd714d996b1f6aff909c2a780db36be22405
julhariemaddin-dev <julharie.maddin.13@gmail.com>
2026-06-18 19:38:08 +0800

    refactor : changes a few things not important

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
               onClick={() => navigate('/products')}
               className={styles.navBtnShop}
             >
-              Shop →
+              Shop
             </button>
           </div>
 
