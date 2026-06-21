9d6e4829c9d1192b19c07c5c1f3fb75f652df898
julhariemaddin-dev <julharie.maddin.13@gmail.com>
2026-06-21 13:27:37 +0800

    refactor : home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -136,7 +136,7 @@ export default function Home() {
               <span className={styles.serverStatusText}>
                 {serverStatus === 'checking' && 'PINGING INSTANCE...'}
                 {serverStatus === 'online' && 'SERVER // ONLINE'}
-                {serverStatus === 'offline' && 'SERVER / UNREACHABLE // Server self hosted in nrok tunnel or local network may be down.'}
+                {serverStatus === 'offline' && 'Homie server is down , backend dev is to broke to afford a vps , try again later or contact the dev for a testing'}
               </span>
             </div>
           </div>
